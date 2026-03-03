96bb084ca1a923bbaeecd9fc068bf02d45af26d7
CarlaC08 <carla.collange@outlook.fr>
2026-03-03 11:29:22 +0100

    Update IR_spectra_processing.py

--- IR_spectra_processing.py
+++ IR_spectra_processing.py
@@ -513,7 +513,7 @@ with visuTab :
                 if submitted and topo_file is not None:
                     if topo_file.name.endswith('.csv'): st.session_state['Topography'] = pd.read_csv(topo_file); st.session_state['Topography_image'] = False
                     elif topo_file.name.endswith('.png') : st.session_state['Topography'] = cv2.imdecode(np.asarray(bytearray(topo_file.read()), dtype=np.uint8), 1); st.session_state['Topography_image'] = True
-                    elif topo_file.name.endswith('.tiff') or topo_file.name.endswith('.tif'): st.session_state['Topography'] = io.imread(topo_file); st.session_state['Topography_image'] = False
+                    elif topo_file.name.endswith('.tiff') or topo_file.name.endswith('.tif'): st.session_state['Topography'] = io.imread(topo_file); st.session_state['Topography_image'] = True
                     elif topo_file.name.endswith('.txt'): st.session_state['Topography'] = np.loadtxt(topo_file, delimiter=';'); st.session_state['Topography_image'] = False
                     st.session_state.topo_submit=True
         with c4 :
@@ -523,7 +523,7 @@ with visuTab :
                 if submitted and IR_file is not None:
                     if IR_file.name.endswith('.png'): st.session_state['IR'] = cv2.imdecode(np.asarray(bytearray(IR_file.read()), dtype=np.uint8), 1); st.session_state['IR_image'] = True
                     elif IR_file.name.endswith('.csv'): st.session_state['IR'] = pd.read_csv(IR_file); st.session_state['IR_image'] = False
-                    elif IR_file.name.endswith('.tiff') or IR_file.name.endswith('.tif'): st.session_state['IR'] = io.imread(IR_file); st.session_state['IR_image'] = False
+                    elif IR_file.name.endswith('.tiff') or IR_file.name.endswith('.tif'): st.session_state['IR'] = io.imread(IR_file); st.session_state['IR_image'] = True
                     elif IR_file.name.endswith('.txt'): st.session_state['IR'] = np.loadtxt(IR_file, delimiter=';'); st.session_state['IR_image'] = False
                     st.session_state.IR_submit=True
         st.write('Size of the map (topography and/or IR) : ',str(st.session_state.map_size[0]),'x',str(st.session_state.map_size[1]),' ',st.session_state.map_unit)
